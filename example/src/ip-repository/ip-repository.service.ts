58edca968710f8bca342fb0d850f4ad3351f2ebd
Deokgyu Yang <secugyu@gmail.com>
2022-06-13 14:54:05 +0900

    Update whitelist IP addresses in the service
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- example/src/ip-repository/ip-repository.service.ts
+++ example/src/ip-repository/ip-repository.service.ts
@@ -4,13 +4,13 @@ import { Injectable } from "@nestjs/common";
 export class IpRepositoryService {
 
   /**
-   * '::1' is from the Postman on macOS
-   * '::ffff:127.0.0.1' is from the `curl` command on iTerm2
+   * '(^::1)' is from the Postman on macOS
+   * '(^192.168.)' is for the local network, using 192.168.0.1/16 subnet mask
    * '127.0.0.1' is a typical localhost IP address
    */
   private whitelistIpAddresses = [
-    '::1',
-    '::ffff:127.0.0.1',
+    '(^::1)',
+    '(^192.168.)',
     '127.0.0.1',
   ];
 
